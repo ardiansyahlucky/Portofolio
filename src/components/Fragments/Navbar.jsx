742e3c18355013580a0f7fe9e24f025b5cc17784
Lucky Ardiansyah <ardiansyahlucky13@gmail.com>
2023-07-28 23:21:45 +0700

    change images

--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -1,10 +1,11 @@
 import Button from "../Elements/Button/Button";
+import WaveImage from "/wave/headerwave.svg";
 
 const Navbar = () => {
   return (
     <div className="container">
       <span className="absolute inset-x-0 -top-10 left-0 -z-20">
-        <img src="/wave/headerwave.svg" alt="" className="hidden md:block" />
+        <img src={WaveImage} alt="" className="hidden md:block" />
       </span>
       <div className=" fixed md:relative bg-opacity-90 bg-lime-500 inset-x-0 top-0 left-0 z-20 ">
         <div className="flex w-full justify-between space-x-2 mx-3 px-6 mt-2 mb-5 text-white">
